9c3381907acc7dd8be8f546c3ce032b6b2939362
Milo Olfindo <142918074+hotcake1@users.noreply.github.com>
2024-07-23 00:15:22 +0800

    Finish exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,22 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    function isLeapYear(){
+        if(year % 4 == 0){
+            if(year % 100 == 0){
+                if(year % 400 == 0){
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return true;
+            }
+        }
+    }
+    if (isLeapYear()) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
