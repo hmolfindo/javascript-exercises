641b870af89687871d153d2b9794d794edcb26b1
Milo Olfindo <142918074+hotcake1@users.noreply.github.com>
2024-07-22 21:22:45 +0800

    Finish exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...item) {
+    return array.filter(value => !item.includes(value));
 };
 
 // Do not edit below this line
