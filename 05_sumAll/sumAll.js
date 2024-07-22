910b8c2077df8027e26b8a4873ef34f84c5e7829
Milo Olfindo <142918074+hotcake1@users.noreply.github.com>
2024-07-23 00:03:10 +0800

    Finish exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    let result = 0;
+    const start = Math.min(startNum, endNum);
+    const finish = Math.max(startNum, endNum);
 
+    function isValid(){
+        if(typeof startNum == 'number' &&
+        typeof endNum == 'number' && 
+        Number.isInteger(startNum) && 
+        Number.isInteger(endNum) &&
+        start > 0 &&
+        finish > 0 ){
+            return true;
+        };
+    }
+
+    if(!isValid()){
+        return "ERROR";
+    }else{
+        for(i = start; i < finish + 1; i++){
+            result += i;
+        }
+    }
+
+    return result;
 };
 
 // Do not edit below this line
