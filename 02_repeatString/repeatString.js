950706b73bb202207a011ba60c48d4c1cf8fd750
Milo Olfindo <142918074+hotcake1@users.noreply.github.com>
2024-07-22 15:56:24 +0800

    Finish exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, times) {
+    if(times < 0){
+        return "ERROR";
+    }else{
+    let result = '';
+    for(i = 0; i < times; i++){
+        result += string;
+    }
+    return result;
+    }
+}
 
 // Do not edit below this line
 module.exports = repeatString;
