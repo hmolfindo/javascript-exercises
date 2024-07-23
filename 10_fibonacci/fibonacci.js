65df3788dbc2e49d823cfa311b5e7245838a4f50
Milo Olfindo <142918074+hotcake1@users.noreply.github.com>
2024-07-23 16:54:29 +0800

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let num1 = 1;
+    let num2 = 0;
+    let sum = 1;
+
+    if(num == 0){
+        return 0;
+    }else if(num < 0){
+        return "OOPS";
+    }else{
+        for(i = 1; i < num; i++){
+            sum = num1 + num2
+            num2 = num1;
+            num1 = sum;
+        }
+        return sum;
+    }
 
 };
 
