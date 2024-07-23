c6f485ca9e12edffe454c09d24d2962f3e822422
Milo Olfindo <142918074+hotcake1@users.noreply.github.com>
2024-07-23 16:13:51 +0800

    Finish exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const validCharacters = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let cleanedString = string
+                        .toLowerCase()
+                        .split("")
+                        .reverse()
+                        .filter((character => validCharacters.includes(character)))
+                        .join("");
+    let result = cleanedString
+                .split("")
+                .reverse()
+                .join("");
 
+    if(result === cleanedString){
+        return true;
+    }else{
+        return false;
+    }
 };
 
 // Do not edit below this line
