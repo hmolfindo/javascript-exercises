fec0bafcae8922e13f5a54edc0b83c9b934e9b36
Milo Olfindo <142918074+hotcake1@users.noreply.github.com>
2024-07-23 12:58:29 +0800

    Finish exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+	let result = 0;
+  for(i = 0; i < numArray.length; i++){
+    result += numArray[i];
+  }
+
+  return result;
 };
 
-const multiply = function() {
+const multiply = function(numArray) {
+  let result = 1;
+  for(i = 0; i < numArray.length; i++){
+    result *= numArray[i];
+  }
 
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = num;
+  if(num === 0 || num ===1){
+    return 1;
+  }else{
+    while(num > 1){
+      num--;
+      result *= num;
+    }
+  }
+
+  return result;
 };
 
 // Do not edit below this line
