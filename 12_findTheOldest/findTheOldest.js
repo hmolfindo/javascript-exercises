cdfd29be4250ca5551f4a2c02964886aefdcfec2
Milo Olfindo <142918074+hotcake1@users.noreply.github.com>
2024-09-02 16:54:46 +0800

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    function getAge(birth, death){
+        if(!death){
+            var death = new Date().getFullYear();
+        }
+            return death - birth;
 
+    }
+    const oldestPerson = array.reduce(  (oldest, person) => 
+                                        getAge(person.yearOfBirth, person.yearOfDeath) < getAge(oldest.yearOfBirth, oldest.yearOfDeath) 
+                                        ? oldest : person);
+    return oldestPerson;
 };
 
 // Do not edit below this line
